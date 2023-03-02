e59596261527c39ac8517ac2ba80176fc7efdd08
leeshroy <98988333+leeshroy@users.noreply.github.com>
2023-03-02 20:14:36 +0000

    changed data types result returns a tuple

--- main.py
+++ main.py
@@ -16,26 +16,26 @@ class WolfGoatCabbage(Problem):
     for item in action:
       #because not existing in state means it is on the other side of the river state observes, we remove to indicate the item has moved to the side we are not observing or adding to indicate the item has moved to the side we are observing
       result_state.remove(item) if item in state else result_state.append(item)
-    result_state = tuple(result_state)
     #returns the new state after actions are taken
+    result_state = tuple(result_state)
     return result_state
 
   def actions(self, state):
-    possible_actions = ()
-    sorted_state = tuple(sorted(state))
+    possible_actions = []
+    sorted_state = sorted(state)
     #check what the current states are and select the possible actions allowed
-    if sorted_state == ('C', 'F', 'G', 'W'):
-      possible_actions = ({'F', 'G'},)
-    elif sorted_state == ('C', 'F', 'G') or sorted_state == ('W',):
-      possible_actions = ({'C', 'F'}, {'F', 'G'})
-    elif sorted_state == ('C', 'F', 'W') or sorted_state == ('G',):
-      possible_actions = ({'C', 'F'}, {'F', 'W'}, {'F'})
-    elif sorted_state == ('C', 'G', 'W') or sorted_state == ('F',):
-      possible_actions = ({'F'},)
-    elif sorted_state == ('F', 'G', 'W') or sorted_state == ('C',):
-      possible_actions = ({'F', 'G'}, {'F', 'W'})
-    elif sorted_state == ('F', 'G') or sorted_state == ('C', 'W'):
-      possible_actions = ({'F', 'G'}, {'F'})
+    if sorted_state == ['C', 'F', 'G', 'W']:
+      possible_actions = [{'F', 'G'}]
+    elif sorted_state == ['C', 'F', 'G'] or sorted_state == ['W']:
+      possible_actions = [{'C', 'F'}, {'F', 'G'}]
+    elif sorted_state == ['C', 'F', 'W'] or sorted_state == ['G']:
+      possible_actions = [{'C', 'F'}, {'F', 'W'}, {'F'}]
+    elif sorted_state == ['C', 'G', 'W'] or sorted_state == ['F']:
+      possible_actions = [{'F'}]
+    elif sorted_state == ['F', 'G', 'W'] or sorted_state == ['C']:
+      possible_actions = [{'F', 'G'}, {'F', 'W'}]
+    elif sorted_state == ['F', 'G'] or sorted_state == ['C', 'W']:
+      possible_actions = [{'F', 'G'}, {'F'}]
     return possible_actions
 
 if __name__ == '__main__':
